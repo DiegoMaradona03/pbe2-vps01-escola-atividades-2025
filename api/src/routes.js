2125ce7df913b13a2b0816ae4add88c3ab83b804
DiegoMaradona03 <diego.mpcfigueredo@gmail.com>
2025-04-15 16:41:50 -0300

    adcionado código do prisma e CRUD

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const routes = express.Router();
 
+const Aluno = require('./controllers/aluno');
+const Telefone = require('./controllers/telefone');
+const Atividade = require('./controllers/atividade');
+
 routes.get('/', (req, res) => {
   return res.json({ titulo: 'Escola ACME' });
 });
+
+routes.post('/alunos', Aluno.create);
+routes.get('/alunos', Aluno.read);
+routes.get('/alunos/:ra', Aluno.readOne);
+routes.put('/alunos/:ra', Aluno.update);
+routes.delete('/alunos/:ra', Aluno.remove);
+
+routes.post('/telefones', Telefone.create);
+routes.get('/telefones', Telefone.read);
+routes.get('/telefones/:id', Telefone.readOne);
+routes.put('/telefones/:id', Telefone.update);
+routes.delete('/telefones/:id', Telefone.remove);
+
+routes.post('/atividades', Atividade.create);
+routes.get('/atividades', Atividade.read);
+routes.get('/atividades/:id', Atividade.readOne);
+routes.put('/atividades/:id', Atividade.update);
+routes.delete('/atividades/:id', Atividade.remove);
 
 module.exports = routes;
